a7101126f92f2952967ff755fa53564816ee6270
MarqHumph <marqhumph@gmail.com>
2026-02-20 19:59:33 -0500

    added logger, circleshape, player and game_state with changes to main

--- main.py
+++ main.py
@@ -3,6 +3,9 @@
 # throughout this file
 import pygame
 from constants import *
+from circleshape import *
+from player import *
+from logger import log_state
 
 def main():
     print("Starting Asteroids!")
@@ -13,13 +16,23 @@ def main():
     print("Screen height: " + str(SCREEN_HEIGHT))
 
     game = True
+    clock = pygame.time.Clock()
+    dt = 0
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2, PLAYER_RADIUS)
+
     while game == True:
+        log_state()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
             
-        screen.fill("black")
+        screen.fill("black") 
+        player.update(dt)
+        player.draw(screen)
+
         pygame.display.flip()
+        clock.tick(60)
+        dt = clock.tick(60) / 1000
 
 
 if __name__ == "__main__":
